d5a68108965535a625009f913e3675792ec3e559
Anubhav Bhetuwal <66554789+abhetu@users.noreply.github.com>
2023-02-07 19:35:57 -0600

    Update First.cpp

--- First.cpp
+++ First.cpp
@@ -1,7 +1,10 @@
-# include <iostream>
-using nampespace std;
-int main(){
-cout << " Hello World!" << endl;
-return 0;
-}
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
